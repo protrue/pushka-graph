9a68d200254e618f435e52d0825c62b6cc6cf088
Anton <3ykovanton@gmail.com>
2019-01-30 10:52:50 +0500

    create gui project

---
+++ PushkaGraphGui/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PushkaGraphCore;
+
+namespace PushkaGraphGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Graph _graph;
+        private Dictionary<IVertex, VertexControl> _vertices;
+        private int _vertexSize;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _graph = new Graph();
+            _vertexSize = 32;
+            _vertices = new Dictionary<IVertex, VertexControl>();
+        }
+
+        private void CreateVertex(object sender, MouseButtonEventArgs e)
+        {
+            var point = e.GetPosition(this);
+            point.Y -= _vertexSize / 2;
+            point.X -= _vertexSize / 2;
+
+            var vertexControl = new VertexControl();
+            Canvas.SetTop(vertexControl, point.Y);
+            Canvas.SetLeft(vertexControl, point.X);
+            Container.Children.Add(vertexControl);
+
+            var vertex = _graph.AddVertex();
+            _vertices[vertex] = vertexControl;
+        }
+    }
+}
