2e50a40992af81fe44d5d1cee7844d29d7e08e48
Anton <3ykovanton@gmail.com>
2019-01-30 10:52:50 +0500

    create gui project

---
+++ PushkaGraphGui/VertexControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PushkaGraphGUI
+{
+    /// <summary>
+    /// Interaction logic for VertexControl.xaml
+    /// </summary>
+    public partial class VertexControl : UserControl
+    {
+        public Brush Color { get; set; }
+        public VertexControl()
+        {
+            InitializeComponent();
+            Width = 32;
+            Height = 32;
+            Color = Brushes.White;
+            Ellipse.SetBinding(Shape.FillProperty, new Binding {Source = Color});
+        }
+    }
+}
