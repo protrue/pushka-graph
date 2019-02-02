e1f6c73d0666101bb868e76e20f5e40041efe583
Anton <3ykovanton@gmail.com>
2019-02-02 18:48:18 +0500

    a lot of changes

---
+++ PushkaGraphGui/VertexSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PushkaGraphGUI
+{
+    public static class VertexSettings
+    {
+        public static readonly Brush BackgroundColor = Brushes.White;
+        public static readonly Brush BoundColor = Brushes.Black;
+        public static readonly int BoundThickness = 2;
+        public static readonly int BoundThicknessHover = 4;
+        public static readonly int Size = 32;
+        public static readonly int ZIndex = 1;
+        public static readonly int MovingZIndex = 2;
+    }
+}
