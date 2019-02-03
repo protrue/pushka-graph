01dcd694cb89b47e4aaa8d984518ddfa0a826ed0
Anton <3ykovanton@gmail.com>
2019-02-03 19:00:24 +0500

    refactoring

--- PushkaGraphGui/InterfaceAction.cs
+++ PushkaGraphGui/InterfaceAction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PushkaGraphGUI
+﻿namespace PushkaGraphGUI
 {
     public enum InterfaceAction
     {
