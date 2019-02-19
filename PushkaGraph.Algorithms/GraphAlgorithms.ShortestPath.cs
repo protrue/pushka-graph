307417353598eb0e698e8bb0c0afe05b9ebb9790
Necenzurnii <xernya97@gmail.com>
2019-02-19 18:55:41 +0500

    Algorithm and Tests of ShortestPath

--- PushkaGraph.Algorithms/GraphAlgorithms.ShortestPath.cs
+++ PushkaGraph.Algorithms/GraphAlgorithms.ShortestPath.cs
@@ -1,6 +1,41 @@
-﻿namespace PushkaGraph.Algorithms
+﻿using PushkaGraph.Core;
+using System.Collections.Generic;
+
+namespace PushkaGraph.Algorithms
 {
     public static partial class GraphAlgorithms
     {
+        /// <summary>
+        /// Extension method example
+        /// </summary>
+        /// <param name="graph">Graph</param>
+        /// <param name="begin">Vertex at start of the Path</param>
+        /// <param name="end">Vertex at end of the Path</param>
+        /// <returns>Vertices</returns>
+        public static IEnumerable<Vertex> ShortestPath(this Graph graph, Vertex begin, Vertex end)
+        {
+            List<Vertex> path = new List<Vertex>();
+            path.Add(begin);
+            Vertex current = begin;
+            Vertex last;
+            int minimalpath = 0;
+            while (current != end)
+            {
+                int length = current.IncidentEdges.Length;
+                int curpath = minimalpath + current.IncidentEdges[0].Weight;
+                int currentId = 0;
+                for (int i = 1; i < length; i++)
+                    if (minimalpath + current.IncidentEdges[i].Weight < curpath)
+                    {
+                        curpath = minimalpath + current.IncidentEdges[i].Weight;
+                        currentId = i;
+                    }
+                minimalpath = curpath;
+                last = current;
+                current = current.GetAdjacentVertexBy(current.IncidentEdges[currentId]);
+                path.Add(current);
+            }
+            return path;
+        }
     }
 }
--- PushkaGraph.Tests/AlgorithmsTests.cs
+++ PushkaGraph.Tests/AlgorithmsTests.cs
@@ -15,5 +15,96 @@ namespace PushkaGraph.Tests
 
             graph.SomeAlgorithm(null).Should().BeNull();
         }
+        [TestMethod]
+        public void ShortestPath_smallgraph()
+        {
+            var graph = new Graph(3);
+
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[1]);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[2]);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[2]);
+
+            graph.ShortestPath(graph.Vertices[2], graph.Vertices[1]).Should().BeNullOrEmpty();
+        }
+        [TestMethod]
+        public void ShortestPath_largegraph()
+        {
+            var graph = new Graph(6);
+
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[1], 7);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[2], 3);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[3], 6);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[4], 2);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[5], 3);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[2], 4);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[4], 6);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[5], 6);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[3], 1);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[4], 4);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[4], 5);
+            graph.AddEdge(graph.Vertices[4], graph.Vertices[5], 4);
+
+            graph.ShortestPath(graph.Vertices[2], graph.Vertices[1]).Should().BeNullOrEmpty();
+        }
+        [TestMethod]
+        public void ShortestPath_fullgraph()
+        {
+            var graph = new Graph(10);
+
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[1], 7);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[2], 3);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[3], 6);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[4], 2);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[5], 3);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[6], 3);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[7], 3);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[8], 3);
+            graph.AddEdge(graph.Vertices[0], graph.Vertices[9], 3);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[2], 4);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[3], 4);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[4], 6);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[5], 6);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[6], 4);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[7], 4);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[8], 4);
+            graph.AddEdge(graph.Vertices[1], graph.Vertices[9], 4);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[3], 1);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[4], 4);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[5], 4);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[6], 4);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[7], 4);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[8], 4);
+            graph.AddEdge(graph.Vertices[2], graph.Vertices[9], 9);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[4], 5);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[5], 5);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[6], 5);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[7], 5);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[8], 5);
+            graph.AddEdge(graph.Vertices[3], graph.Vertices[9], 5);
+            graph.AddEdge(graph.Vertices[4], graph.Vertices[5], 4);
+            graph.AddEdge(graph.Vertices[4], graph.Vertices[6], 4);
+            graph.AddEdge(graph.Vertices[4], graph.Vertices[7], 4);
+            graph.AddEdge(graph.Vertices[4], graph.Vertices[8], 4);
+            graph.AddEdge(graph.Vertices[4], graph.Vertices[9], 4);
+            graph.AddEdge(graph.Vertices[5], graph.Vertices[6], 4);
+            graph.AddEdge(graph.Vertices[5], graph.Vertices[7], 4);
+            graph.AddEdge(graph.Vertices[5], graph.Vertices[8], 4);
+            graph.AddEdge(graph.Vertices[5], graph.Vertices[9], 4);
+            graph.AddEdge(graph.Vertices[6], graph.Vertices[7], 4);
+            graph.AddEdge(graph.Vertices[6], graph.Vertices[8], 4);
+            graph.AddEdge(graph.Vertices[6], graph.Vertices[9], 4);
+            graph.AddEdge(graph.Vertices[7], graph.Vertices[8], 4);
+            graph.AddEdge(graph.Vertices[7], graph.Vertices[9], 4);
+            graph.AddEdge(graph.Vertices[8], graph.Vertices[9], 4);
+
+            graph.ShortestPath(graph.Vertices[2], graph.Vertices[9]).Should().BeNullOrEmpty();
+        }
+        [TestMethod]
+        public void ShortestPath_Emptygraph()
+        {
+            var graph = new Graph(10);
+            
+            graph.ShortestPath(graph.Vertices[2], graph.Vertices[9]).Should().BeNullOrEmpty();
+        }
     }
 }
