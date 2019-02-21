91b4bc68671548bb60312cb274362c161cce1cde
George Korolev <polecater@mail.ru>
2019-02-21 12:40:29 +0500

    Add wrapper for shortest path
    Fix other wrappers
    Add tests for MST

--- PushkaGraph.NewAlgorithms/Implementations/ShortestPath.cs
+++ PushkaGraph.NewAlgorithms/Implementations/ShortestPath.cs
@@ -9,29 +9,38 @@ using System.Threading.Tasks;
 
 namespace PushkaGraph.NewAlgorithms.Implementations
 {
-    class ShortestPath : GraphAlgorithm
+    public class ShortestPath : GraphAlgorithm
     {
         public override string Name => "Кратчайший путь";
         public override string Description => "Поиск кратчайшего пути между точками в графе";
 
         // Указываем типы, которые алгоритм принимает на вход как аргументы
-        public override Type[] RequiredParameterTypes => new[] { typeof(Graph), typeof(Vertex[]) };
+        public override Tuple<Type, int>[] RequiredParameters => new[]
+        {
+            Tuple.Create(typeof(Graph), 1),
+            Tuple.Create(typeof(Vertex), 1)
+        };
 
         // Указываем типы, которые алгоритм возвращает как результат своего выполнения
-        public override Type[] ResultTypes => new[] { typeof(IEnumerable<Vertex>) };
+        public override Type[] ResultTypes => new[] { typeof(Vertex[]) };
 
         protected override GraphAlgorithmResult PerformAlgorithm(GraphAlgorithmParameters parameters)
         {
             // Извлекаем из параметров нужные штуки
             var graph = parameters.Graph;
             var begin = graph.Vertices[0];
-            var end = graph.Vertices[4];
+            var end = graph.Vertices[1];
 
             // Вызываем написанный extension метод
-            var VertexPath = GraphAlgorithms.ShortestPath(graph,begin,end);
+            var shortestPath = graph.ShortestPath(begin, end)?.ToArray();
+
+            var stringResult =
+                shortestPath != null
+                ? $"Кратчайший путь: {string.Join<Vertex>(Environment.NewLine, shortestPath)}"
+                : "Нет пути";
 
             // Оборачиваем результат выполнения
-            var result = new GraphAlgorithmResult(vertices: VertexPath.ToArray());
+            var result = new GraphAlgorithmResult(vertices: shortestPath, stringResult: stringResult);
 
             // Возвращаем
             return result;
